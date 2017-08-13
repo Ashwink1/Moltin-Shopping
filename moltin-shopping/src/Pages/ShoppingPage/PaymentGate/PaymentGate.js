0ffcaa9426d4722b881495fda165edcc5b7b3e40
ashwinkrishna <ashwin.krishna@razorthink.net>
2017-08-14 01:12:05 +0530

    Changes for loader css

--- moltin-shopping/src/Pages/ShoppingPage/PaymentGate/PaymentGate.js
+++ moltin-shopping/src/Pages/ShoppingPage/PaymentGate/PaymentGate.js
@@ -3,20 +3,27 @@ import PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import CircularProgress from 'material-ui/CircularProgress';
+import './PaymenGate.css';
+
+const Loader = () => {
+  return <div className="loader-wrapper"><div className="loader-center" ><CircularProgress size={60} thickness={7}/></div></div>;
+};
 
 class PaymentGate extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      username:'',
-      password:''
-    }
+    this.state = {
+      loader: false,
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(){
-  /*  this.props.moltin.Order.Payment(order.id, {
+  handleClick() {
+
+    this.setState({ loader: true });
+   /* this.props.moltin.Order.Payment(order.id, {
       gateway: 'stripe',
       method: 'purchase',
       first_name: 'John',
@@ -24,9 +31,13 @@ class PaymentGate extends Component {
       number: '4242424242424242',
       month: '02',
       year: '2020',
-      verification_value: '123'
+      verification_value: '123',
     });*/
-    this.props.match.history.push('/');
+
+    setTimeout(()=>{
+      this.setState({ loader: false });
+      this.props.match.history.push('/');
+    },7000)
   }
 
   render() {
@@ -38,10 +49,11 @@ class PaymentGate extends Component {
             <TextField
               hintText="otp"
               floatingLabelText="otp"
-              onChange = {(event,newValue) => this.setState({otp:newValue})}
+              onChange={(event, newValue) => this.setState({ otp: newValue })}
             />
             <br/>
-            <RaisedButton label="Pay now" primary={true}  onClick={(event) => this.handleClick(event)}/>
+            <RaisedButton label="Pay now" primary={true} onClick={(event) => this.handleClick(event)}/>
+            {this.state.loader && <Loader />}
           </div>
         </MuiThemeProvider>
       </div>
