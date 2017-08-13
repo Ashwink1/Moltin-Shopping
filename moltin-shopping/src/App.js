4a29a6c114bb9354dd5a374b7f36ae49985d51a3
ashwinkrishna <ashwin.krishna@razorthink.net>
2017-08-13 16:18:00 +0530

    Rout Setup

--- moltin-shopping/src/App.js
+++ moltin-shopping/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
 import './App.css';
 import Login from './Pages/Login/Login';
+import ShoppingPage from './Pages/ShoppingPage/ShoppingPage';
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>Page Not found <code>{location.pathname}</code></h3>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+        <Router>
+          <Switch>
+            <Route path="/" exact component={ShoppingPage}/>
+            <Route path="/login" component={Login}/>
+            <Route component={NoMatch}/>
+          </Switch>
+        </Router>
       </div>
     );
   }
--- moltin-shopping/src/Pages/ShoppingPage/ShoppingPage.js
+++ moltin-shopping/src/Pages/ShoppingPage/ShoppingPage.js
@@ -3,12 +3,43 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+const Home = () => (
+  <p>
+    A <code>&lt;Switch></code> renders the
+    first child <code>&lt;Route></code> that
+    matches. A <code>&lt;Route></code> with
+    no <code>path</code> always matches.
+  </p>
+)
+
+const WillMatch = () => <h3>Matched!</h3>
+
+
 
 class ShoppingPage extends Component {
   render() {
     return (
       <div>
-        Hai
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/will-match">Will Match</Link></li>
+            </ul>
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/will-match" component={WillMatch}/>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
