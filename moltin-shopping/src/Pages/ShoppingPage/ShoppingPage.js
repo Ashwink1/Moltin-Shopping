a006d15a64e2ebb4a472f4f5593380849c0f2183
ashwinkrishna <ashwin.krishna@razorthink.net>
2017-08-13 15:29:14 +0530

    Shopping page added

---
+++ moltin-shopping/src/Pages/ShoppingPage/ShoppingPage.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Ashwin on 13/08/17.
+ */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ShoppingPage extends Component {
+  render() {
+    return (
+      <div>
+        Hai
+      </div>
+    );
+  }
+}
+
+ShoppingPage.propTypes = {};
+ShoppingPage.defaultProps = {};
+
+export default ShoppingPage;
+
