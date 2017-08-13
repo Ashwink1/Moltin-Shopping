8b3c8717ad4e3947cc96d2d98571b2d14e813e77
ashwinkrishna <ashwin.krishna@razorthink.net>
2017-08-13 15:25:48 +0530

    Login page included

---
+++ moltin-shopping/src/Pages/Login/Login.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Ashwin on 13/08/17.
+ */
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import React from 'react';
+
+const style = {
+  margin: 15,
+};
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username:'',
+      password:''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event){
+    const payload={
+      "email":this.state.username,
+      "password":this.state.password
+    }
+    // do the auth here
+    console.log("Authenthicated");
+
+  }
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              title="Login"
+            />
+            <TextField
+              hintText="Enter your Username"
+              floatingLabelText="Username"
+              onChange = {(event,newValue) => this.setState({username:newValue})}
+            />
+            <br/>
+            <TextField
+              type="password"
+              hintText="Enter your Password"
+              floatingLabelText="Password"
+              onChange = {(event,newValue) => this.setState({password:newValue})}
+            />
+            <br/>
+            <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+export default Login;
